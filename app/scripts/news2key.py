e9940fb90b6cfe08ee6ea1db867771bf3e985027
Foundsheep <moondeokjong@gmail.com>
2024-05-09 14:42:22 +0900

    fix: get law names before keybert model, so top_n could be kept at the end

--- app/scripts/news2key.py
+++ app/scripts/news2key.py
@@ -6,7 +6,24 @@ def run(url_list, threshold=0.0, top_n=10):
     keywords_list = []
     for url in url_list:
         print(f"{url = }")
+
+        # TODO: try, except to catch the url provided doesn't match the naver news html structure
         text = get_text_from_url(url)
+        # law names by regex
+        regex_law_names = get_law_words_by_regex(text)
+
+        # law names by json
+        json_law_names = get_law_words_by_json(text)
+
+        # get a new top_n
+        law_names = list(set(regex_law_names + json_law_names))
+        original_top_n = top_n
+        top_n -= len(law_names)
+        if top_n < 1 :
+            print(f"the number of pre-found law_names : [{len(law_names)}] >= original top_n : [{original_top_n}]")
+            top_n = 10
+            print(f"So top_n is set to [{top_n}] and will output [{top_n + len(law_names)}] keywords, if they all are above threshold")
+
         keywords_tuple = kw_model.extract_keywords(text, vectorizer=vectorizer, top_n=top_n)
         
         keywords = []
@@ -17,16 +34,8 @@ def run(url_list, threshold=0.0, top_n=10):
             else:
                 print(f"[{tup[0]}] keyword has been excluded with threshold :[{tup[1]}]")
 
-        # law names by regex
-        regex_law_names = get_law_words_by_regex(text)
-
-        # law names by json
-        json_law_names = get_law_words_by_json(text)
-
         # combine them all
-        # TODO: change the logic 1. to achieve top_n 
-        # TODO: 2. check if law names in keywords before 1
-        keywords = keywords + regex_law_names + json_law_names
+        keywords = keywords + law_names
         keywords = list(set(keywords))
 
         concatenated_keywords = ",".join(keywords)
