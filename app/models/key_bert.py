55156df9611492f9864fd7cfae2b1252243e7ca3
Foundsheep <moondeokjong@gmail.com>
2024-05-28 16:35:04 +0900

    feat: load a model not from huggingface but from local files

--- app/models/key_bert.py
+++ app/models/key_bert.py
@@ -1,12 +1,13 @@
 from transformers import BertModel, AutoTokenizer, AutoModelForCausalLM
 from keybert import KeyBERT
+import os
 
 
 def count_parameters(model):
     return sum(p.numel() for p in model.parameters() if p.requires_grad)
 
-# TODO: Get the model from local
 def get_keybert():
-    model = BertModel.from_pretrained('skt/kobert-base-v1')
+    model_dir = os.path.join(os.path.dirname(__file__), "skt/kobert-base-v1")
+    model = BertModel.from_pretrained(model_dir, local_files_only=True)
     kw_model = KeyBERT(model)
     return kw_model
--- app/models/ko_sim.py
+++ app/models/ko_sim.py
@@ -1,8 +1,9 @@
 from transformers import AutoModel, AutoTokenizer
+import os
 
 def get_ko_sim_models():
-
-    model = AutoModel.from_pretrained('BM-K/KoSimCSE-roberta')
+    model_dir = os.path.join(os.path.dirname(__file__), 'BM-K/KoSimCSE-roberta')
+    model = AutoModel.from_pretrained(model_dir, local_files_only=True)
     tokenizer = AutoTokenizer.from_pretrained('BM-K/KoSimCSE-roberta')
 
     return tokenizer, model
