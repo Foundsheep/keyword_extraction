58ff05f7bdf78a8a6161fbb980a3a5b1e8783680
Foundsheep <moondeokjong@gmail.com>
2024-05-29 17:31:09 +0900

    feat: words_recommendation with naver api and filtering based on embedding similarity

--- app/configuration.py
+++ app/configuration.py
@@ -2,8 +2,11 @@ from dataclasses import dataclass
 
 @dataclass
 class Config:
-    # KR_DICT_API_KEY: str = "E46D906ADBB4387D1E4EFCD343F2D489"
-
     # https://aiopen.etri.re.kr/guide/Word
     KR_WISE_WORD_NET_API_KEY = "ee1810c8-dc20-4698-8208-1c5adb7fe4b9"
     OPEN_API_URL = "http://aiopen.etri.re.kr:8000/WiseWWN/Word"
+    NAVER_BASE_URL = 'https://api.searchad.naver.com'
+    NAVER_URI = '/keywordstool'
+    NAVER_API_KEY = '01000000007d0e897d57e7af5bb1a4222e54870198a42d10e6a2f5ed3bb3a1adbacd5bea4c'
+    NAVER_SECRET_KEY = 'AQAAAAB9Dol9V+evW7GkIi5UhwGY29uIyBvHW4+EKuFc05Zq2A=='
+    NAVER_CUSTOMER_ID = '1801275'
--- app/utils/util_words_recommendation.py
+++ app/utils/util_words_recommendation.py
@@ -2,7 +2,14 @@ from ..configuration import Config
 from . import http
 import json
 
-def get_similar_words_from_wwn_api(word):
+import hashlib
+import hmac
+import base64
+
+import time
+import requests
+
+def get_similar_words_from_wwn_api(word, top_n):
 
     print(f"word to api : [{word}]")
     return_list = []
@@ -35,12 +42,21 @@ def get_similar_words_from_wwn_api(word):
                     hypernyms.extend(word_info["Hypernym"])
                     hypornyms.extend(word_info["Hypornym"])
 
-            return_list.extend(synonyms)
-            print(f"synonyms for word[{word}] = {synonyms}")
-            return_list.extend(hypernyms)
-            print(f"hypernyms for word[{word}] = {hypernyms}")
-            return_list.extend(hypornyms)
-            print(f"hypornyms for word[{word}] = {hypornyms}")
+            # 1단계 유사어
+            return_list.extend(synonyms[:top_n])
+            print(f"synonyms for word[{word}] = {synonyms[:top_n]}")
+
+            # 2단계 상위어
+            if len(return_list) < top_n:
+                new_top_n = top_n - len(return_list)
+                return_list.extend(hypernyms[:new_top_n])
+                print(f"hypernyms for word[{word}] = {hypernyms[:new_top_n]}")
+            
+            # 3단계 하위어
+            if len(return_list) < top_n:
+                new_top_n = top_n - len(return_list)
+                return_list.extend(hypornyms[:new_top_n])
+                print(f"hypornyms for word[{word}] = {hypornyms[:new_top_n]}")
 
             # 정제
             return_list = [w.split("_")[0] for w in return_list]
@@ -51,3 +67,62 @@ def get_similar_words_from_wwn_api(word):
         print(f"open api call failed. Call status = [{response.status}]")
 
     return return_list    
+
+
+# naver api 활용을 위한 클래스
+class Signature:
+
+    @staticmethod
+    def generate(timestamp, method, uri, secret_key):
+        message = "{}.{}.{}".format(timestamp, method, uri)
+        hash = hmac.new(bytes(secret_key, "utf-8"), bytes(message, "utf-8"), hashlib.sha256)
+
+        hash.hexdigest()
+        return base64.b64encode(hash.digest())
+
+def get_similar_words_from_naver_ad_api(word, top_n=10):
+
+    def _get_header(method, uri, api_key, secret_key, customer_id):
+        timestamp = str(round(time.time() * 1000))
+        signature = Signature.generate(timestamp, method, uri, Config.NAVER_SECRET_KEY)
+        return {
+            'Content-Type': 'application/json; charset=UTF-8', 
+            'X-Timestamp': timestamp, 
+            'X-API-KEY': Config.NAVER_API_KEY, 
+            'X-Customer': str(Config.NAVER_CUSTOMER_ID), 
+            'X-Signature': signature
+            }
+    
+    return_list = []
+
+    method = 'GET'
+
+    params={}
+    params['hintKeywords']=word
+    params['showDetail']='1'
+
+    r=requests.get(Config.NAVER_BASE_URL + Config.NAVER_URI, 
+                   params=params, 
+                   headers=_get_header(method, 
+                                       Config.NAVER_URI, 
+                                       Config.NAVER_API_KEY,
+                                       Config.NAVER_SECRET_KEY,
+                                       Config.NAVER_CUSTOMER_ID))
+
+    if r.status_code == 200:
+        print(f"naver api call suceeded")
+
+        j = r.json()
+        keywords = j["keywordList"][1:] # 본인 단어는 제외
+        try:
+            for i in range(top_n):
+                rel_keyword = keywords[i]["relKeyword"]
+                return_list.append(rel_keyword)
+        except Exception as e:
+            print(e)
+            print(f"[{i+1}]th element caused an error. {len(return_list) = }")
+    
+    else:
+        print(f"{r.status_code = }")
+    
+    return return_list
