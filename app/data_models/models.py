f3691994e8dd9cf47ca4b50a416237b4278a151a
Foundsheep <moondeokjong@gmail.com>
2024-05-30 11:18:22 +0900

    fix: data models' default values

--- app/data_models/models.py
+++ app/data_models/models.py
@@ -3,16 +3,18 @@ from typing import Union, List
 
 class News2Keywords(BaseModel):
     url_list: Union[List[str], None] = None
-    threshold: float = None
-    top_n: int = None
+    threshold: float = 0.0
+    top_n: int = 10
 
 class Sentence2Keywords(BaseModel):
     text_list: Union[List[str], None] = None
-    threshold: float = None
-    top_n: int = None
+    threshold: float = 0.0
+    top_n: int = 10
 
 class SimilarWords(BaseModel):
     text: str = None
+    threshold: float = 0.5
+    top_n: int = 10
 
 class EmbeddingSimilarity(BaseModel):
     text_1: str = None
--- app/scripts/keywords_from_sentences.py
+++ app/scripts/keywords_from_sentences.py
@@ -1,6 +1,6 @@
 from ..utils.util_keywords_extraction import extract_keywords_by_keybert_with_law_names
 
-def run(text_list, threshold=0.0, top_n=10):
+def run(text_list, threshold, top_n):
     keywords_list = []
     for text in text_list:
         keywords = extract_keywords_by_keybert_with_law_names(text, top_n, threshold)
