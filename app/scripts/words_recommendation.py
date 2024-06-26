fix: context similarity calculation logic in words_recommendation modified

app/scripts/words_recommendation.py
@@ -1,4 +1,6 @@
 import traceback
+import numpy as np
+
 from ..utils.util_keywords_extraction import tokenize_kr
 from ..utils.util_words_recommendation import get_similar_words_from_naver_ad_api, get_similar_words_from_wwn_api
 from . import contexts_similarity as cs
@@ -8,16 +10,18 @@ def run(text, threshold=0.3, top_n=10):
     try:
         # 하나의 문장 형태로 들어온다면 tokenize
         if len(text.strip().split()) > 1:
+            print(f"input text has more than 1 word, length = {len(text.strip().split())}")
             tokens = tokenize_kr(text)
             
             # 의미 있는 단어만 선별
-            words = []
             print(f"1nd stage... {len(tokens) = }")
 
-            for token in tokens:
-                score = cs.run(text, token)
-                if score >= threshold:
-                    words.append(token)
+            scores = cs.run([text], tokens)
+            scores = np.array(scores)
+            indices = np.where(scores >= threshold)[0]
+            tokens_arr = np.array(tokens)
+            words = list(tokens_arr[indices])
+
             print(f"After 1nd stage... {len(words) = }")
         else:
             words = [text]
@@ -40,13 +44,17 @@ def run(text, threshold=0.3, top_n=10):
             keyword_list.extend(wwn_keywords)
 
         print(f"2nd stage... {len(keyword_list) = }")
-        return_list = []
-        for recommended_word in keyword_list:
-            score = cs.run(text, recommended_word)
-            if score >= threshold:
-                return_list.append(recommended_word)
-                print(f"recommended_word({recommended_word}) added in the final list with score [{score :.2f}]")
-        print(f"After 2nd stage... {len(return_list) = }")        
+
+        # 유사도 기반으로 적합한 것만 추출
+        scores_final = cs.run([text], keyword_list)
+        scores_final = np.array(scores_final)
+        indices_final = np.where(scores_final >= threshold)[0]
+        tokens_arr_final = np.array(keyword_list)
+        return_list = list(tokens_arr_final[indices_final])
+
+        print(f"After 2nd stage... {len(return_list) = }")
+
+        # top_n 개만 추출
         return_list = return_list[:top_n]
     
     except Exception as e: